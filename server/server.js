e038567f8fe45a4a23cccc94fb8bc103c72610cb
Luis <luisriveraphi@gmail.com>
2024-05-20 21:51:02 -0400

    new commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
+
+// Need to create './schemas'
+// const { typeDefs, resolvers } = require('./schemas');
+
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3000;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
---
+++ server/utils/auth.js
@@ -0,0 +1,17 @@
+const { GraphQlError } = require("graphql");
+const jwt = require("jsonwebtoken");
+
+const secret = "thisisasecretpassword";
+const expiration = "2hr";
+
+module.exports = {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    extensions: {
+      code: "UNAUTHENTICATED",
+    },
+  }),
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
